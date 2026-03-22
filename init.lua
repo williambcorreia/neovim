da26913d0e53a228c0623f4091a0cad254499dd8
William Correia <wburgard6@gmail.com>
2026-03-22 12:31:19 -0300

    removed transparency

--- init.lua
+++ init.lua
@@ -9,15 +9,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.showmode = false
 
--- transparency
+-- theme
 vim.cmd.colorscheme("gruvbox") 
-local function set_transparency()
-    local groups = { "Normal", "NormalFloat", "SignColumn", "NormalNC", "MsgArea" }
-    for _, group in ipairs(groups) do
-        vim.api.nvim_set_hl(0, group, { bg = "none" })
-    end
-end
-set_transparency()
 
 -- bindings
 vim.g.mapleader = " "
@@ -27,3 +20,4 @@ vim.keymap.set ('n', '<leader>t', ':tabnew<CR>', { silent = true })
 vim.keymap.set ('n', '<leader>qt', ':tabclose<CR>', { silent = true })
 vim.keymap.set ('n', '<leader>ht', ':tabprevious<CR>', { silent = true })
 vim.keymap.set ('n', '<leader>lt', ':tabnext<CR>', { silent = true })
+vim.keymap.set ('n', '<leader>c', 'set_transparency()', { silent = true })
