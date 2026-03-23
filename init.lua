f78922882d4221a76406e867b8c70abd76ed48c6
William Correia <wburgard6@gmail.com>
2026-03-22 22:37:30 -0300

    require >> lua/config.lua (split cfg files)

--- init.lua
+++ init.lua
@@ -1,23 +1,4 @@
 -- options
 require("config.lazy")
-vim.opt.syntax = "on"
-vim.opt.encoding = "utf-8"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = ""
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.showmode = false
-
--- theme
-vim.cmd.colorscheme("gruvbox") 
-
--- bindings
-vim.g.mapleader = " "
-vim.keymap.set ('n', '<leader>e', ':Ex<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>s', ':w | %so<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>t', ':tabnew<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>qt', ':tabclose<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>ht', ':tabprevious<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>lt', ':tabnext<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>c', 'set_transparency()', { silent = true })
+require("binds")
+require("opts")
