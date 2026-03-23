d379d0ac8f78b74aeaed789af8cd31246d57ba26
William Correia <wburgard6@gmail.com>
2026-03-22 22:38:27 -0300

    vim options separate file

---
+++ lua/opts.lua
@@ -0,0 +1,12 @@
+-- options
+vim.opt.syntax = "on"
+vim.opt.encoding = "utf-8"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = ""
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmode = false
+
+-- theme
+vim.cmd.colorscheme("gruvbox") 
