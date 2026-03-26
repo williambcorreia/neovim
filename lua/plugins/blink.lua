918f853eb6e27b752d72043810a6c0df0e0d45bd
William Correia <wburgard6@gmail.com>
2026-03-26 14:35:32 -0300

    change cmp menu and preset to super-tab on blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return {
   'saghen/blink.cmp',
   version = '*',
   opts = {
-    keymap = { preset = 'enter' },
+    keymap = { preset = 'super-tab' },
 
     appearance = {
       nerd_font_variant = 'mono'
