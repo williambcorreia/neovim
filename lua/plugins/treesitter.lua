2b57cdcc07ce9cc2565d3b46f8890c660d1c73b7
William Correia <wburgard6@gmail.com>
2026-03-26 15:06:54 -0300

    remove treesitter.lua plugin

--- lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate',
-  config = function ()
-	  require('nvim-treesitter').setup{}
-	  require('nvim-treesitter').install { 'python', 'c', 'kotlin' }
-	end,
-}
