f053d1bc8ae1d64ddcb9f120a2395f3be0cc028f
William Correia <wburgard6@gmail.com>
2026-03-26 12:54:36 -0300

    other configs on binds and opts, add plugins

--- lua/binds.lua
+++ lua/binds.lua
@@ -1,9 +1,6 @@
 -- bindings
 vim.g.mapleader = " "
-vim.keymap.set ('n', '<leader>ex', ':Ex<CR>', { silent = true }) 
-vim.keymap.set ('n', '<leader>s', ':w | %so<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>t', ':tabnew<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>et', ':tabnew | Ex<CR>', { silent = true })
+vim.keymap.set ('n', '<leader>t', ':tabnew | Ex<CR>', { silent = true })
 vim.keymap.set ('n', '<leader>qt', ':tabclose<CR>', { silent = true })
 vim.keymap.set ('n', '<leader>ht', ':tabprevious<CR>', { silent = true })
 vim.keymap.set ('n', '<leader>lt', ':tabnext<CR>', { silent = true })
