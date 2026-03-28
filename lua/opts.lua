6aab74f6ee90b1985bd27d55e31db9b5ee2c78a5
William Correia <wburgard6@gmail.com>
2026-03-27 23:01:42 -0300

    add split below and split right options

--- lua/opts.lua
+++ lua/opts.lua
@@ -7,6 +7,8 @@ vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.showmode = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true 
 vim.diagnostic.enable(false)
 
 -- theme
