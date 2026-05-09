3eecfc06a6eca44ce6c55464af348a1c061b60dd
William Correia <wburgard6@gmail.com>
2026-05-08 23:29:21 -0300

    new config on one file

--- lua/binds.lua
+++
@@ -1,8 +0,0 @@
--- bindings
-vim.g.mapleader = " "
-vim.keymap.set ('n', '<leader>t', ':tabnew | Ex<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>qt', ':tabclose<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>ht', ':tabprevious<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>lt', ':tabnext<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>sv', ':new<CR>', { silent = true })
-vim.keymap.set ('n', '<leader>sh', ':vnew<CR>', { silent = true })
